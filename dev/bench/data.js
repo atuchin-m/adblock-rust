a70910c943a0d7a3f08253f72f0b009ea25cc462
github-action-benchmark <github@users.noreply.github.com>
2025-01-23 17:35:48 +0000

    add Rust Benchmark (cargo) benchmark result for 49c8b0518b15b7a4c2d8915190f86f579d0a1bbd

---
+++ dev/bench/data.js
@@ -0,0 +1,34 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1737653748902,
+  "repoUrl": "https://github.com/atuchin-m/adblock-rust",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "atuchin-m",
+            "username": "atuchin-m"
+          },
+          "committer": {
+            "name": "atuchin-m",
+            "username": "atuchin-m"
+          },
+          "id": "49c8b0518b15b7a4c2d8915190f86f579d0a1bbd",
+          "message": "[benchmark] setup perf actions",
+          "timestamp": "2025-01-23T17:04:21Z",
+          "url": "https://github.com/atuchin-m/adblock-rust/pull/1/commits/49c8b0518b15b7a4c2d8915190f86f579d0a1bbd"
+        },
+        "date": 1737653748083,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "rule-match/easylist",
+            "value": 1468315439,
+            "range": "± 30357922",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
